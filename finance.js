12dd3fb77b868b4e9e4b1e31a9e2330baa272a5d
franciscojoaopedro <franciscojoaopedro1998@gmail.com>
2022-09-16 21:04:54 +0100

    happy happy

--- finance.js
+++ finance.js
@@ -1,4 +1,9 @@
-const app=document.querySelector('#app')
+const app=document.querySelector('#app');
+const btnLancamento=document.querySelector('#btn-lancamento');
+const valor=document.querySelector('#valor');
+const categoria=document.querySelector('#categoria');
+const tipo=document.querySelector('#tipo');
+const mes=document.querySelector('#mes');
 // function calcularSaldo(mes,saldoIncial,lancamentos){
 //     console.log(mes)
 //     const totalDoMes={
@@ -47,39 +52,34 @@ let saldoIncial=0;
 const janeiro= new Mes("janeiro");
 janeiro.adicionarLancamento(new Lancamento ("Salario","receita",3000))
 janeiro.adicionarLancamento(new Lancamento("Aluguel","despesa",1000))
-janeiro.adicionarLancamento(new Lancamento("Conta de Luz","despesa",200))
-janeiro.adicionarLancamento(new Lancamento("Conta de Água","despesa",100))
-janeiro.adicionarLancamento(new Lancamento("Internet","despesa",100))
-janeiro.adicionarLancamento(new Lancamento("Transporte","despesa",300))
-janeiro.adicionarLancamento(new Lancamento("Lazer","despesa",300))
-janeiro.adicionarLancamento(new Lancamento("Alimentação","despesa",500))
-janeiro.adicionarLancamento(new Lancamento("Condomínio","despesa",300))
-janeiro.adicionarLancamento(new Lancamento("Farmácia","despesa",100))
-
-
-
+// janeiro.adicionarLancamento(new Lancamento("Conta de Luz","despesa",200))
+// janeiro.adicionarLancamento(new Lancamento("Conta de Água","despesa",100))
+// janeiro.adicionarLancamento(new Lancamento("Internet","despesa",100))
+// janeiro.adicionarLancamento(new Lancamento("Transporte","despesa",300))
+// janeiro.adicionarLancamento(new Lancamento("Lazer","despesa",300))
+// janeiro.adicionarLancamento(new Lancamento("Alimentação","despesa",500))
+// janeiro.adicionarLancamento(new Lancamento("Condomínio","despesa",300))
+// janeiro.adicionarLancamento(new Lancamento("Farmácia","despesa",100))
 
 const fevereiro=  new Mes("favereiro");
 fevereiro.adicionarLancamento(new Lancamento("Salario","receita",3000))
 fevereiro.adicionarLancamento(new Lancamento("Aluguel","despesa",1200))
 fevereiro.adicionarLancamento(new Lancamento("Conta de Luz","despesa",250))
-fevereiro.adicionarLancamento(new Lancamento("Conta de Água","despesa",100))
-fevereiro.adicionarLancamento(new Lancamento("Internet","despesa",100))
-fevereiro.adicionarLancamento(new Lancamento("Transporte","despesa",500))
-fevereiro.adicionarLancamento(new Lancamento("Alimentação","despesa",1000))
-fevereiro.adicionarLancamento(new Lancamento("Condomínio","despesa",400))
-
-
+// fevereiro.adicionarLancamento(new Lancamento("Conta de Água","despesa",100))
+// fevereiro.adicionarLancamento(new Lancamento("Internet","despesa",100))
+// fevereiro.adicionarLancamento(new Lancamento("Transporte","despesa",500))
+// fevereiro.adicionarLancamento(new Lancamento("Alimentação","despesa",1000))
+// fevereiro.adicionarLancamento(new Lancamento("Condomínio","despesa",400))
 const marco=  new Mes("março");
 marco.adicionarLancamento(new Lancamento("Salario","receita",4000))
 marco.adicionarLancamento(new Lancamento("Aluguel","despesa",1200))
 marco.adicionarLancamento(new Lancamento("Conta de Luz","despesa",200))
 marco.adicionarLancamento(new Lancamento("Conta de Água","despesa",100))
-marco.adicionarLancamento(new Lancamento("Internet","despesa",200))
-marco.adicionarLancamento(new Lancamento("Transporte","despesa",500))
-marco.adicionarLancamento(new Lancamento("Lazer","despesa",800));
-marco.adicionarLancamento(new Lancamento("Alimentação","despesa",1000));
-marco.adicionarLancamento(new Lancamento("Condomínio","despesa",400));
+// marco.adicionarLancamento(new Lancamento("Internet","despesa",200))
+// marco.adicionarLancamento(new Lancamento("Transporte","despesa",500))
+// marco.adicionarLancamento(new Lancamento("Lazer","despesa",800));
+// marco.adicionarLancamento(new Lancamento("Alimentação","despesa",1000));
+// marco.adicionarLancamento(new Lancamento("Condomínio","despesa",400));
 
 
 const ano=new Ano()
@@ -104,18 +104,39 @@ const addElement=(parent,elementType,text)=>{
 }
 
 const renderizar=()=>{
-    for(const mes of ano.meses){
-        addElement(app,"h3",mes.nome)
-        for(const lancamento of mes.lancamentos){
-            addElement(app,'p',`${lancamento.tipo} :${lancamento.valor}  ${lancamento.categoria}`) 
-        }
-        addElement(app,"h4",mes.totalizador.saldo)
-        addElement(app,"hr")
+    if(app.firstChild){
+        app.firstChild.remove();
     }
+    const painel=document.createElement('div');
+    for(const mes of ano.meses){
+        addElement(painel,"h3",mes.nome)
+        for(const lancamento of mes.lancamentos){
+            addElement(painel,'p',`${lancamento.tipo} :${lancamento.valor}  ${lancamento.categoria}`) 
+        }
+        addElement(painel,"h4",mes.totalizador.saldo)
+        addElement(painel,"hr")
+    }
+    app.appendChild(painel)
 }
-renderizar()
 
+renderizar();
+const apagarInputsValue=()=>{
+    mes.value="";
+    tipo.value="";
+    valor.value="";
+    categoria.value="";
+}
+const adicionarLancamento=()=>{
+    ano.adicionarLancamento(mes.value,new Lancamento(categoria.value,tipo.value,parseFloat(valor.value)))
+    ano.calcularSaldo()
+    renderizar()
+    apagarInputsValue();
+}
+btnLancamento.addEventListener('click',(event)=>{
+    event.preventDefault();
+    adicionarLancamento()
 
+})
 
 
 
